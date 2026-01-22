a79f8873f833294ebc9b6ac5f3b46b1273b03643
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2026-01-22 15:54:41 +0800

    fix: Fix router

--- ui/src/router/modules/trigger.ts
+++ ui/src/router/modules/trigger.ts
@@ -4,25 +4,23 @@ const ModelRouter = {
   name: 'trigger',
   meta: {
     title: 'views.trigger.title',
-    menu: true,
     permission: [
       RoleConst.USER.getWorkspaceRole,
       RoleConst.WORKSPACE_MANAGE.getWorkspaceRole,
       PermissionConst.TOOL_READ.getWorkspacePermission,
       PermissionConst.TOOL_READ.getWorkspacePermissionWorkspaceManageRole,
     ],
-    icon: 'app-tool',
-    iconActive: 'app-tool-active',
     group: 'workspace',
     order: 5,
   },
+  hidden: true,
   redirect: '/trigger',
   component: () => import('@/layout/layout-template/SimpleLayout.vue'),
   children: [
     {
       path: '/trigger',
       name: 'trigger-index',
-      meta: { title: '工具主页', activeMenu: '/trigger' },
+      meta: { title: '触发器主页', activeMenu: '/trigger' },
       sameRoute: 'trigger',
       component: () => import('@/views/trigger/index.vue'),
     },
